79236a63e460ba8ee06e10ca13d5fde5c356b024
krutuja9 <krutuja609@gmail.com>
2024-03-01 17:43:27 +0530

    sign up login pages

---
+++ src/pages/SignupPage.js
@@ -0,0 +1,10 @@
+import SignUp from "../features/auth/components/SignUp";
+function SignupPage() {
+  return ( 
+    <div>
+      <SignUp></SignUp>
+    </div>
+   );
+}
+
+export default SignupPage;
