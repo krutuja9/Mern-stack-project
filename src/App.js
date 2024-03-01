a4080732b821cc557b911809fe14e646028f51d8
krutuja9 <krutuja609@gmail.com>
2024-03-01 21:22:11 +0530

    Cart added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Counter } from './features/counter/Counter';
 import Home from './pages/Home';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
+import CartPage from './pages/CartPage';
 
 import {
   createBrowserRouter,
@@ -25,6 +26,10 @@ const router = createBrowserRouter([
   {
     path: "/signup",
     element: <SignupPage></SignupPage>,
+  },
+  {
+    path: "/cart",
+    element: <CartPage ></CartPage>,
   },
 ]);
 
